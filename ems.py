110110cf9792c2fe1017d87d70fd700f054c4d7c
Yashu_Kohli11 <89150253+yashu1wwww@users.noreply.github.com>
2022-06-03 17:23:04 +0530

    Add files via upload

--- ems.py
+++ ems.py
@@ -1,20 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select 
+from selenium.webdriver.support.ui import Select
 import time
 
 driver = webdriver.Chrome()
-driver.get("http://164.100.133.129:82/tutresult/")
+
+driver.get("http://164.100.133.129:82/tutresult//")
 
 input=driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_txtReg"]')
-input.send_keys('19B11000')
+input.send_keys('19B11096') #put your reg no
 
-input=driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_ddlExam"]')
-input.click()
+input=driver.find_element_by_css_selector('#ContentPlaceHolder1_ddlExam').click()
 
-time.sleep(5)
+time.sleep(2)
 
-#select the sem which you want and leave it it will click view button auto working on drop handle button if i find i will update
+dropdown = Select(driver.find_element_by_id("ContentPlaceHolder1_ddlExam"))
+dropdown.select_by_value("CBBCAD") #i putted 4th sem vlaue put your required > 1st sem-CBBCAA 2nd sem-CBBCAB 3rd sem-CBBCAC 5th sem-CBBCAE 6th sem-CBBCAF
 
 input=driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_btnview"]')
 input.click()
+
+time.sleep(3)
 
